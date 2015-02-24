da13e51417fd147d702f46049e9c8b34f5c8d98f
Robin ROIGNANT <rroignan@e1r4p21.42.fr>
2015-02-24 21:22:19 +0100

    stat.c

--- stat.c
+++ stat.c
@@ -6,7 +6,7 @@
 /*   By: rroignan <rroignan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/10 17:17:47 by rroignan          #+#    #+#             */
-/*   Updated: 2015/02/23 20:07:17 by rroignan         ###   ########.fr       */
+/*   Updated: 2015/02/24 19:28:12 by rroignan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,22 @@
 #include <grp.h>
 #include "libft.h"
 
-char		*ft_adjust(char *time)
+void				lsymb(char *arg)
 {
-	int		i;
-	int		j;
-	char	*cpy;
+	char			*symb;
+
+	symb = ft_strnew(256);
+	readlink(arg, symb, 256);
+	ft_putstr(arg);
+	ft_putstr(" -> ");
+	ft_putendl(symb);
+}
+
+char				*ft_adjust(char *time)
+{
+	int				i;
+	int				j;
+	char			*cpy;
 
 	i = 0;
 	j = 0;
@@ -41,29 +52,29 @@ char		*ft_adjust(char *time)
 	return (cpy);
 }
 
-static char		ft_type(mode_t buf)
+static char			ft_type(mode_t buf)
 {
-		if (S_ISREG(buf))
-			return('-');
-		else if (S_ISDIR(buf))
-			return('d');
-		else if (S_ISCHR(buf))
-			return('c');
-		else if (S_ISBLK(buf))
-			return('b');
-		else if (S_ISFIFO(buf))
-			return('p');
-		else if (S_ISLNK(buf))
-			return('l');
-		else if (S_ISSOCK(buf))
-			return('s');
-		else if (S_ISWHT(buf))
-			return('w');
-		else
-			return ('?');
+	if (S_ISREG(buf))
+		return ('-');
+	else if (S_ISDIR(buf))
+		return ('d');
+	else if (S_ISCHR(buf))
+		return ('c');
+	else if (S_ISBLK(buf))
+		return ('b');
+	else if (S_ISFIFO(buf))
+		return ('p');
+	else if (S_ISLNK(buf))
+		return ('l');
+	else if (S_ISSOCK(buf))
+		return ('s');
+	else if (S_ISWHT(buf))
+		return ('w');
+	else
+		return ('?');
 }
 
-static char		*ft_rights(char *str, mode_t buf)
+static char			*ft_ri(char *str, mode_t buf)
 {
 	str[0] = ft_type(buf);
 	str[1] = buf & S_IRUSR ? 'r' : '-';
@@ -76,53 +87,32 @@ static char		*ft_rights(char *str, mode_t buf)
 	str[8] = buf & S_IWOTH ? 'w' : '-';
 	str[9] = buf & S_IXOTH ? 'x' : '-';
 	str[10] = '\0';
-
 	return (str);
 }
 
-int		main(int ac, char **av)
+int					main(int ac, char **av)
 {
-	char			*rights;
-	char			*symb;
+	char			*ri;
 	struct stat		buf;
 	struct passwd	*name;
 	struct group	*grp;
 
-	rights = ft_strnew(11);
+	ri = ft_strnew(11);
 	if (ac == 2)
 	{
 		lstat(av[1], &buf);
 		name = getpwuid(buf.st_uid);
 		grp = getgrgid(buf.st_gid);
-		//ft_putstr("Type de fichier: ");
-		ft_putstr(ft_rights(rights, buf.st_mode));
-		ft_putstr("  ");
-		//ft_putstr("Nombre de lien: ");
+		ft_putstr_ls(ft_ri(ri, buf.st_mode));
 		ft_putnbr(buf.st_nlink);
 		ft_putstr(" ");
-		//ft_putstr("Propriétaire: ");
-		ft_putstr(name->pw_name);
-		ft_putstr("  ");
-		//ft_putstr("Groupe: ");
-		ft_putstr(grp->gr_name);
-		ft_putstr("  ");
-		//ft_putstr("Taille: ");
+		ft_putstr_ls(name->pw_name);
+		ft_putstr_ls(grp->gr_name);
 		ft_putnbr(buf.st_size);
 		ft_putstr(" ");
-		//ft_putstr("Date de derniere modification: ");
 		ft_putstr(ft_adjust(ctime(&(buf.st_mtimespec.tv_sec))));
 		ft_putstr(" ");
-		//ft_putstr("Nom du fichier: ");
-		if (ft_rights(rights, buf.st_mode)[0] == 'l')
-		{
-			symb = ft_strnew(256);
-			readlink(av[1], symb, 256);
-			ft_putstr(av[1]);
-			ft_putstr(" -> ");
-			ft_putendl(symb);
-		}
-		else
-			ft_putstr(av[1]);
+		(ft_ri(ri, buf.st_mode)[0] == 'l' ? lsymb(av[1]) : ft_putendl(av[1]));
 	}
 	return (0);
 }
