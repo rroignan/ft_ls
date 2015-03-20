ec8750a81f6e2c7063d90af3c7492f8ee8e96bd4
Robin ROIGNANT <rroignan@e1r4p22.42.fr>
2015-03-20 19:08:01 +0100

    oui

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: rroignan <rroignan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/24 21:18:06 by rroignan          #+#    #+#             */
-/*   Updated: 2015/03/12 16:56:02 by rroignan         ###   ########.fr       */
+/*   Updated: 2015/03/20 19:07:20 by rroignan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 void	print_l(t_list *list)
 {
-	ft_stat(list);
-	ft_putstr(list->group);
+	//ft_stat(list);
 	ft_putstr(list->mode);
-	ft_putnbr(list.nblink);
-	ft_putnbr(list.size);
-	ft_putstr(list->time);
+	ft_putstr("  ");
+	ft_putnbr(list->nblink);
+	ft_putstr(" ");
 	ft_putstr(list->uid);
+	ft_putstr(" ");
+	ft_putstr(list->group);
+	ft_putstr(" ");
+	ft_putnbr(list->size);
+	ft_putstr(" ");
+	ft_putstr(list->time);
+	ft_putstr(" ");
+	ft_putendl(list->name);
 }
 
 void	print_list(t_list *list, char *arg)
@@ -31,7 +38,7 @@ void	print_list(t_list *list, char *arg)
 	{
 		while (list)
 		{
-			ft_putendl(list->phr);
+			ft_putendl(list->name);
 			list = list->next;
 		}
 	}
@@ -39,11 +46,11 @@ void	print_list(t_list *list, char *arg)
 	{
 		while (list)
 		{
-			if (list->phr[0] == '.')
+			if (list->name[0] == '.')
 				list = list->next;
 			else
 			{
-				ft_putendl(list->phr);
+				ft_putendl(list->name);
 				list = list->next;
 			}
 		}
@@ -58,7 +65,8 @@ t_list	*add_link(t_list *list, char *str)
 	tmp = malloc(sizeof(t_list));
 	if (tmp)
 	{
-		tmp->phr = str;
+		tmp->name = str;
+		ft_stat(tmp);
 		tmp->next = NULL;
 	}
 	if (list == NULL)
@@ -73,23 +81,56 @@ t_list	*add_link(t_list *list, char *str)
 	}
 }
 
+char	**fill_tab(char **av)
+{
+	int		i;
+	int		r;
+	int		f;
+	char	**tab;
+
+	i = 0;
+	r = 0;
+	f = 1;
+	while (av[i] && av[i + 1][0] == '-')
+		i++;
+	if ((tab = (char **)malloc(sizeof(char *) * (i + 1))) == NULL)
+		return (NULL);
+	i = 0;
+	while (av[f][0] == '-')
+	{
+		tab[r] = ft_strnew(ft_strlen(av[f]));
+		ft_strcpy(tab[r], av[f]);
+		r++;
+		f++;
+	}
+	tab[r] = '\0';
+	return (tab);
+}
+
 int		main(int ac, char **av)
 {
-	struct dirent	*library;
+	flag_l	*flag;
+	char 	**tab;
+	int		r;
+
+	tab = fill_tab(av);
+	flag = ft_check_f(tab);
+	/*struct dirent	*library;
 	DIR				*dirp;
 	t_list			*list;
 
 	list = NULL;
 	ac = ac;
-	dirp = opendir(".");
-	/*if (dirp == NULL)
+	dirp = opendir(av[2]);
+	if (dirp == NULL)
 	{
 		ft_putendl(av[1]);
 		return (0);
-	}*/		
+	}		
 	while ((library = readdir(dirp)) != NULL)
 		list = add_link(list, library->d_name);
 	(list != NULL ? print_list(list, av[1]) : list);
-	closedir(dirp);
+	closedir(dirp);*/
+	ac = ac;
 	return (0);
 }
